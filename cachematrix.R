3a0c600f16e92798c3707e2836b03eb251093a2e
Kazbek <nmb.ten@gmail.com>
2014-04-20 11:30:03 +0400

    module documentation added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,41 @@
+## Matrix inversion is usually a costly computation and their may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## This two functions do exactly that. Create matrix that stores its inverse (using makeCacheMatrix)
+## then get its inverse (using  cacheSolve)
+##
+## Minimal example:
+##
+## > mc <- makeCacheMatrix(mtx=matrix(c(1, 2, 3, 4), 2, 2))
+##
+## > cacheSolve(mc)  # first call, computing and set to cache
+##     [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+##
+## > cacheSolve(mc)  # second call, getting from cache
+##     [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+## Matrix inversion is usually a costly computation and their may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## This two functions do exactly that. Create matrix that stores its inverse (using makeCacheMatrix)
+## then get its inverse (using  cacheSolve)
+##
+## Minimal example:
+##
+## > mc <- makeCacheMatrix(mtx=matrix(c(1, 2, 3, 4), 2, 2))
+##
+## > cacheSolve(mc)  # first call, computing and set to cache
+##     [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+##
+## > cacheSolve(mc)  # second call, getting from cache
+##     [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+
 # makeCacheMatrix
 # returns matrix object that stores its inverse
 
